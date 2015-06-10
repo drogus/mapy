4c753937e01d4b7236c5feb0255c9f4598743a19
Jakub Bukaj <jakub.adam.bukaj@gmail.com>
2015-06-10 22:38:35 +0200

    Daty spotkań

--- generate.rb
+++ generate.rb
@@ -1,24 +1,39 @@
 require 'geocoder'
+require 'i18n'
+
+I18n.available_locales = [:pl]
+I18n.locale = :pl
+I18n.load_path += ['./locale.yml']
 
 Geocoder.configure(:lookup => :yandex)
 
-coordinates = Hash[ *ARGF.lines.map(&:strip).map do |line|
-    [line, Geocoder.coordinates(line)]
-end.flatten(1) ]
-valid_coordinates = coordinates.reject { |_, value| value.nil? }
+lines = ARGF.lines.map(&:strip).map { |line| line.split("\t") }.to_a
+coordinates = Hash[ *lines.map { |line|
+    location = line[0]
+    next_meeting = if line[1].nil?
+        nil
+    else
+        localized = I18n.localize(Date.parse(line[1]), :format => "%e %B %Y")
+        "Następne spotkanie: #{localized}"
+    end
+    Geocoder.search(location + ", Poland").map { |result|
+        [result.coordinates.reverse, {
+            :title => result.city,
+            :description => next_meeting
+        }.reject { |k, v| v.nil? }]
+    }.first
+}.reject(&:nil?).flatten(1) ]
 
 puts JSON.pretty_generate({
     :type => "FeatureCollection",
-    :features => valid_coordinates.map { |location, coordinates|
+    :features => coordinates.map { |coordinates, properties|
         {
             :type => "Feature",
             :geometry => {
                 :type => "Point",
-                :coordinates => coordinates.reverse
+                :coordinates => coordinates
             },
-            :properties => {
-                :title => location
-            }
+            :properties => properties
         }
     }
 })
