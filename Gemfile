0b8ae9b8e3ec4037d452c4fb998271178e8f245a
Jakub Bukaj <jakub.adam.bukaj@gmail.com>
2015-06-11 20:01:28 +0200

    Powiaty.

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source "https://rubygems.org"
 
 gem 'geocoder'
 gem 'i18n'
+gem 'parallel'
 gem 'redcarpet'
--- generate.rb
+++ generate.rb
@@ -1,5 +1,7 @@
 require 'geocoder'
 require 'i18n'
+require 'open-uri'
+require 'parallel'
 require 'redcarpet'
 
 I18n.available_locales = [:pl]
@@ -16,25 +18,34 @@ Geocoder.configure(config)
 
 markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
 
+powiats = Parallel.map(1..379, :in_threads => 32) { |idx|
+    JSON.parse(open("https://api.mojepanstwo.pl/dane/powiaty/#{idx}/geojson").read)
+}
+
 lines = ARGF
     .each_line
     .map(&:rstrip)
     .slice_when { |before, after| !after.match(/^\s/) }
     .to_a
 
-coordinates = Hash[ *lines.map { |line|
+coordinates = lines.map { |line|
     city, country = line[0].sub(/\*/, "").split(',').map(&:strip)
     country ||= 'Poland'
     marker_color = if line[0].start_with?('*') then "#b7167f" else "#5c0b40" end
-    description = markdown.render(line.drop(1).map(&:lstrip).join('\n'))
-    Geocoder.search([city, country].join(', ')).map { |result|
+    description = markdown.render(line
+        .drop(1)
+        .map { |line| line[1..-1] }
+        .join('\n'))
+    Geocoder.search("#{city}, #{country}").map { |result|
         [result.coordinates.reverse, {
             :title => result.city,
             :description => if description.empty? then nil else description end,
             "marker-color" => marker_color
         }.reject { |k, v| v.nil? }]
     }.first
-}.reject(&:nil?).flatten(1) ]
+}
+
+coordinates = Hash[ *coordinates.reject(&:nil?).flatten(1) ]
 
 puts JSON.pretty_generate({
     :type => "FeatureCollection",
@@ -47,5 +58,5 @@ puts JSON.pretty_generate({
             },
             :properties => properties
         }
-    }
+    } + powiats
 })
